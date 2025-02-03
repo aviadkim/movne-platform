de13f420f88388b5e1e232d5ead52f0b38035b2e
aviadkim <aviad@kimfo-fs.com>
2025-02-03 18:11:59 +0200

    Update Conversations.js

--- movne-platform/client/src/Conversations.js
+++ movne-platform/client/src/Conversations.js
@@ -1,19 +1,18 @@
-// client/src/Conversations.js
 import React from 'react';
 import AudioRecorder from './AudioRecorder';
 import RecordingControls from './RecordingControls';
 import TranscriptViewer from './TranscriptViewer';
 import SummaryAndActions from './SummaryAndActions';
 import NeedsQuestionnaire from './NeedsQuestionnaire';
-import LiveTranscript from './LiveTranscript';
+import LiveTranscript from './LiveTranscript';  // אם יש לך את הרכיב הזה, לפי הקוד הקודם
 import RegulatoryQuestions from './RegulatoryQuestions';
-import TestPanel from './TestPanel'; // יבוא הרכיב
+import TestPanel from './TestPanel'; // אם יצרת אותו
 
 function Conversations() {
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">מודול שיחות</h2>
-      <TestPanel /> {/* כפתור בדיקה */}
+      {/* אם תרצה, תוכל לכלול כאן גם TestPanel */}
       <AudioRecorder />
       <RecordingControls />
       <LiveTranscript />
@@ -21,6 +20,9 @@ function Conversations() {
       <SummaryAndActions />
       <NeedsQuestionnaire />
       <RegulatoryQuestions />
+      {/*
+      <TestPanel />  // אפשר להוסיף כפתור בדיקה
+      */}
     </div>
   );
 }
