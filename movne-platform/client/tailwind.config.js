c0b412a1df3168d236ca0d15abeee37c51fe6278
aviadkim <aviad@kimfo-fs.com>
2025-02-04 13:49:09 +0200

    Update tailwind.config.js

--- movne-platform/client/tailwind.config.js
+++ movne-platform/client/tailwind.config.js
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#4CAF50',    // ירוק יוקרתי
+        secondary: '#ff5722',  // כתום
+      },
+    },
   },
-  plugins: []
+  plugins: [
+    require('tailwindcss-rtl'),
+  ],
 }
